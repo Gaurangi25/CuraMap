91ed9e49cfd35ad9a0eeac35389d7b60200ea361
Gaurangi25 <gaurangi250704@gmail.com>
2025-07-09 05:48:43 +0530

    Initial Commit for CuraMap

---
+++ server/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("CuraMap is running");
+});
+
+/* MongoDB Connection */
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  })
+
+  .catch((error) => {
+    console.log("MongoDB connection error : ", error);
+  });
